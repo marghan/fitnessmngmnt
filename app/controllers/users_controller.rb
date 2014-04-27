2d7dd02071209e1bfbe3d338e7e4f28d43232e82
Maggie <hansen4050@gmail.com>
2014-04-27 18:19:43 -0400

    adding graphs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,13 @@ class UsersController < ApplicationController
   def show
    		@user = User.find(params[:id])
       @microposts = @user.microposts.paginate(page: params[:page])
+      #treat this as json
+      @data = [
+              {:x=>2, :y=>10},
+              {x:5, y:12}
+              ].to_json
+  #need to convert data from Ruby to Json
+  #when pulling data from database
  	end
 
   def new
