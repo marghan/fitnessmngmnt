8e1e0556c9c8eb6a969d0233e08717ebde130888
Rich Ungsunan <ungsunan@gmail.com>
2014-05-11 23:42:20 -0400

    added signup email confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.signup_confirmation(@user).deliver 
       sign_in @user
       flash[:success] = "Welcome to fIT Life!"
       redirect_to @user
